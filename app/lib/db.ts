a935806e2b706a77428bcfcc59ca5bfd7b0cdcec
Tejasp2003 <tejasp200320@gmail.com>
2025-07-16 15:25:50 +0530

    correct code

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,20 +1,15 @@
+
 import mongoose from "mongoose";
-
-const MONGODB_URI = process.env.NEXT_PUBLIC_MONGODB_URI!;
-console.log("MONGODB_URI", MONGODB_URI);
-
+const MONGODB_URI = process.env.MONGODB_URI!;
+console.log("MONGODB_URI", process.env.MONGODB_URI);
 console.log("NEXTAUTH_SECRET", process.env.NEXTAUTH_SECRET);
-
 if (!MONGODB_URI) {
   throw new Error("Please define mongo_uri in env variables");
 }
-
 let cached = global.mongoose;
-
 if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
-
 export async function connectToDatabase() {
   if (cached.conn) {
     return cached.conn;
